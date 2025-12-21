21c61787cd0591370ea37c48b367ce362a47edb4
MrSIHAB <mrsihab@hotmail.com>
2025-12-21 21:56:41 +0600

    show hide settings for quick sites

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,12 @@ import { renderTime } from "./timeLoader.js";
 import { initSearch } from "./search.js";
 import { loadShortCutSites } from "./shortcuts.js";
 import { initUiListeners } from "./ui.js";
-import { loadColorSelectionBackground } from "./settings.js";
+import { initSettings } from "./settings.js";
 import { setTheme } from "./theme.js";
 
 renderTime(); // render current date and days
 initSearch(); // Load search bar scripts
 initUiListeners(); // Load eventListeners for all ui
 loadShortCutSites(); // Render shortcut sites and it's listeners
-loadColorSelectionBackground();
+initSettings();
 setTheme();
--- scripts/settings.js
+++ scripts/settings.js
@@ -1,11 +1,18 @@
 import { setTheme } from "./theme.js";
 
+export function initSettings() {
+  document.addEventListener("DOMContentLoaded", () => {
+    _loadColorSelectionBackground();
+    _quickSitesSettings();
+  });
+}
+
 /**
  * Color selection tool is just a selection with some color values.
  * This function will load their background color. The background color
  * will be exactly what is their value.
  */
-export function loadColorSelectionBackground() {
+function _loadColorSelectionBackground() {
   const colorInputs = document.getElementsByName("color");
   colorInputs.forEach(function (element) {
     // setting background
@@ -14,4 +21,44 @@ export function loadColorSelectionBackground() {
     // Listen click event
     element.addEventListener("click", () => setTheme(element.value));
   });
+}
+
+/**
+ * Quick sites settings event listeners. The material buttons to display or hide
+ * sites category. Social sites are default and can't be removed as of now.
+ *
+ * This function not only register the event listeners, but also complete the initial
+ * setup.
+ */
+function _quickSitesSettings() {
+  applyThemeAndListeners("toggleDeveloperSites", "developerQuickSites", false);
+  applyThemeAndListeners("toggleGoogleSites", "googleQuickSites", true);
+  applyThemeAndListeners("toggleMicrosoftSites", "microsoftQuickSites", true);
+
+  /**
+   * Reuseable function to load Initial UI and add listeners
+   * @param {string} switchElementId
+   * @param {string} siteElementId
+   * @param {string} defaultValue
+   */
+  async function applyThemeAndListeners(switchElementId, siteElementId, defaultValue = true) {
+    const switchElement = document.getElementById(switchElementId);
+    const siteElement = document.getElementById(siteElementId);
+
+    // load saved settings
+    const isLocallyEnabled = (localStorage.getItem(switchElementId) ?? defaultValue.toString()) == "true";
+
+    // Set state on first load
+    switchElement.checked = isLocallyEnabled;
+    if (isLocallyEnabled) siteElement.classList.remove("hide");
+    else siteElement.classList.add("hide");
+
+    // add event listener
+    switchElement.addEventListener("click", () => {
+      const isEnabled = switchElement.checked;
+      localStorage.setItem(switchElementId, isEnabled);
+      if (isEnabled) siteElement.classList.remove("hide");
+      else siteElement.classList.add("hide");
+    });
+  }
 }
